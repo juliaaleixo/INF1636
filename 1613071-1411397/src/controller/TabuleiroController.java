97776b96b6880426eafe0ff1c3ddcbcd74ce92ab
juliaaleixo <julia.pereira1995@outlook.com>
2018-06-19 11:27:15 -0300

    .

--- 1613071-1411397/src/controller/TabuleiroController.java
+++ 1613071-1411397/src/controller/TabuleiroController.java
@@ -24,11 +24,8 @@ import model.Rei;
 import model.Torre;
 import view.Janela;
 
-public class TabuleiroController
-		implements
-			MouseListener,
-			Observer,
-			ActionListener {
+public class TabuleiroController implements MouseListener, Observer, ActionListener 
+{
 	/**
 	 * 
 	 */
@@ -38,13 +35,17 @@ public class TabuleiroController
 	public Jogo jogo;
 	Janela janela;
 
-	public TabuleiroController() {
+	public TabuleiroController() 
+	{
 		jogo = new Jogo();
 		janela = new Janela(jogo);
 
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; j++) {
-				if (jogo.tabuleiro[i][j] != null) {
+		for (int i = 0; i < 8; i++) 
+		{
+			for (int j = 0; j < 8; j++) 
+			{
+				if (jogo.tabuleiro[i][j] != null)
+				{
 					jogo.tabuleiro[i][j].addObserver(this);
 					jogo.tabuleiro[i][j].addObserver(this.janela.tabuleiro);
 				}
@@ -76,30 +77,41 @@ public class TabuleiroController
 
 	}
 
-	private void movimentaPeca() {
+	private void movimentaPeca() 
+	{
 		janela.tabuleiro.jogo = jogo;
-		if (!jogo.movimentado) {
+		
+		if (!jogo.movimentado) 
+		{
 			jogo.movimentado = true;
 		}
-		try {
-			jogo.tabuleiro[xOrig][yOrig].movimento(xOrig, yOrig, xDest, yDest,
-					jogo.tabuleiro);
+		try 
+		{
+			jogo.tabuleiro[xOrig][yOrig].movimento(xOrig, yOrig, xDest, yDest, jogo.tabuleiro);
 
-			if (jogo.rodadaAtual == Cor.branco) {
+			if (jogo.rodadaAtual == Cor.branco)
+			{
 				jogo.rodadaAtual = Cor.preto;
-			} else {
+			} 
+			else 
+			{
 				jogo.rodadaAtual = Cor.branco;
 			}
 
 			Boolean[][] reisEmXeque = reisEmXeque(jogo.tabuleiro);
-			for (int i = 0; i < 8; i++) {
-				for (int j = 0; j < 8; j++) {
-					if (reisEmXeque[i][j] == true) {
+			for (int i = 0; i < 8; i++)
+			{
+				for (int j = 0; j < 8; j++) 
+				{
+					if (reisEmXeque[i][j] == true) 
+					{
 						JOptionPane.showMessageDialog(null, "Xeque");
 					}
 				}
 			}
-		} catch (MovIlegalExcecao e) {
+		} 
+		catch (MovIlegalExcecao e) 
+		{
 			return;
 		}
 	}
@@ -116,45 +128,52 @@ public class TabuleiroController
 		return 7 - ((y - 48) / 50);
 	}
 
-	public void mousePressed(MouseEvent e) {
+	public void mousePressed(MouseEvent e)
+	{
 		System.out.println("X: " + e.getX());
 		System.out.println("X: " + e.getY());
 
-		if (pecaSelecionada == false) {
+		if (pecaSelecionada == false) 
+		{
 			xOrig = getTileX(e.getX());
 			yOrig = getTileY(e.getY());
 
-			if (jogo.tabuleiro[xOrig][yOrig] != null) {
-				if (jogo.tabuleiro[xOrig][yOrig].getCor() != jogo.rodadaAtual) {
-					System.out.println(
-							"A jogada e dxs " + jogo.rodadaAtual.toString());
+			if (jogo.tabuleiro[xOrig][yOrig] != null)
+			{
+				if (jogo.tabuleiro[xOrig][yOrig].getCor() != jogo.rodadaAtual) 
+				{
+					System.out.println("A jogada e dxs " + jogo.rodadaAtual.toString());
 					return;
 				}
 				pecaSelecionada = true;
 
-				Boolean[][] posicoes = posicoesPossiveis(jogo.tabuleiro, xOrig,
-						yOrig);
+				Boolean[][] posicoes = posicoesPossiveis(jogo.tabuleiro, xOrig, yOrig);
 				janela.tabuleiro.setPosicoesPossiveis(posicoes);
 
 			}
-		} else {
+		} 
+		else 
+		{
 			int x = getTileX(e.getX());
 			int y = getTileY(e.getY());
 
-			if (jogo.tabuleiro[x][y] != null) {
-				if (jogo.tabuleiro[x][y]
-						.getCor() == jogo.tabuleiro[xOrig][yOrig].getCor()) {
+			if ( jogo.tabuleiro[x][y] != null )
+			{
+				if ( jogo.tabuleiro[x][y].getCor() == jogo.tabuleiro[xOrig][yOrig].getCor() )
+				{
 					xOrig = x;
 					yOrig = y;
 
-					Boolean[][] posicoes = posicoesPossiveis(jogo.tabuleiro,
-							xOrig, yOrig);
+					Boolean[][] posicoes = posicoesPossiveis(jogo.tabuleiro, xOrig, yOrig);
 					janela.tabuleiro.setPosicoesPossiveis(posicoes);
-				} else {
+				} 
+				else 
+				{
 					xDest = x;
 					yDest = y;
 
-					if (!jogo.reiDesprotegido(xOrig, yOrig, xDest, yDest)) {
+					if ( !jogo.reiDesprotegido(xOrig, yOrig, xDest, yDest) )
+					{
 						janela.tabuleiro.setPosicoesPossiveis(null);
 						movimentaPeca();
 
@@ -162,11 +181,14 @@ public class TabuleiroController
 					}
 				}
 
-			} else {
+			} 
+			else
+			{
 				xDest = x;
 				yDest = y;
 
-				if (!jogo.reiDesprotegido(xOrig, yOrig, xDest, yDest)) {
+				if ( !jogo.reiDesprotegido(xOrig, yOrig, xDest, yDest) ) 
+				{
 					janela.tabuleiro.setPosicoesPossiveis(null);
 					movimentaPeca();
 
@@ -178,28 +200,29 @@ public class TabuleiroController
 
 	}
 
-	public void mouseReleased(MouseEvent e) {
-	}
-	public void mouseClicked(MouseEvent e) {
-	}
-	public void mouseEntered(MouseEvent e) {
-	}
-	public void mouseExited(MouseEvent e) {
-	}
+	public void mouseReleased(MouseEvent e) {}
+	public void mouseClicked(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
 
-	public Boolean[][] posicoesPossiveis(Peca[][] tabuleiro, int xOrig,
-			int yOrig) {
+	public Boolean[][] posicoesPossiveis(Peca[][] tabuleiro, int xOrig, int yOrig) 
+	{
 		Boolean posicoesPossiveis[][] = new Boolean[8][8];
 
 		Peca p = tabuleiro[xOrig][yOrig];
 		Cor cor = p.getCor();
 
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; j++) {
+		for (int i = 0; i < 8; i++) 
+		{
+			for (int j = 0; j < 8; j++) 
+			{
 
-				if (p.caminhoLivre(xOrig, yOrig, i, j, tabuleiro)) {
+				if (p.caminhoLivre(xOrig, yOrig, i, j, tabuleiro)) 
+				{
 					posicoesPossiveis[i][j] = true;
-				} else {
+				} 
+				else 
+				{
 					posicoesPossiveis[i][j] = false;
 				}
 			}
@@ -207,16 +230,22 @@ public class TabuleiroController
 		return posicoesPossiveis;
 	}
 
-	public Boolean[][] reisEmXeque(Peca[][] tabuleiro) {
+	public Boolean[][] reisEmXeque(Peca[][] tabuleiro) 
+	{
 		Boolean reisEmXeque[][] = new Boolean[8][8];
 
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; j++) {
-				if (tabuleiro[i][j] != null) {
+		for (int i = 0; i < 8; i++) 
+		{
+			for (int j = 0; j < 8; j++) 
+			{
+				if (tabuleiro[i][j] != null) 
+				{
 					Peca p = tabuleiro[i][j];
 
-					if (p instanceof Rei) {
-						if (((Rei) p).xeque(i, j, tabuleiro, p.getCor())) {
+					if (p instanceof Rei) 
+					{
+						if (((Rei) p).xeque(i, j, tabuleiro, p.getCor()))
+						{
 							reisEmXeque[i][j] = true;
 							continue;
 						}
@@ -237,22 +266,31 @@ public class TabuleiroController
 		}
 	}
 
-	public void promocao(int x, int y) {
-		Object[] possibilities = {"Rainha", "Torre", "Bispo", "Cavalo"};
+	public void promocao(int x, int y) 
+	{
+		Object[] possibilities = { "Rainha", "Torre", "Bispo", "Cavalo" };
+		
 		String s = (String) JOptionPane.showInputDialog(null,
 				"Escolha a nova peca:\n", "Promocao do Peao",
 				JOptionPane.PLAIN_MESSAGE, null, possibilities, "Rainha");
 
-		if ((s != null) && (s.length() > 0)) {
+		if ((s != null) && (s.length() > 0)) 
+		{
 			Peca p = jogo.tabuleiro[x][y];
 
-			if (s.equals("Cavalo")) {
+			if (s.equals("Cavalo")) 
+			{
 				jogo.tabuleiro[x][y] = new Cavalo(p.getCor());
-			} else if (s.equals("Torre")) {
+			} 
+			else if (s.equals("Torre")) 
+			{
 				jogo.tabuleiro[x][y] = new Torre(p.getCor());
-			} else if (s.equals("Bispo")) {
+			} 
+			else if (s.equals("Bispo"))
+			{
 				jogo.tabuleiro[x][y] = new Bispo(p.getCor());
-			} else {
+			} else 
+			{
 				jogo.tabuleiro[x][y] = new Rainha(p.getCor());
 			}
 			jogo.tabuleiro[x][y].addObserver(this);
@@ -284,7 +322,7 @@ public class TabuleiroController
 
 			if (jogo.movimentado) {
 				int result = JOptionPane.showConfirmDialog(janela,
-						"Tem certeza que deseja começar um novo jogo?\nO progresso atual será perdido.",
+						"Tem certeza que deseja comeï¿½ar um novo jogo?\nO progresso atual serï¿½ perdido.",
 						"Novo jogo", JOptionPane.YES_NO_OPTION);
 
 				if (result == 0) {
--- 1613071-1411397/src/view/Tabuleiro.java
+++ 1613071-1411397/src/view/Tabuleiro.java
@@ -38,10 +38,12 @@ public class Tabuleiro extends JPanel implements Observer {
 	BufferedImage imagemReiBranco, imagemReiPreto;
 	BufferedImage imagemTorreBranca, imagemTorrePreta;
 
-	public Tabuleiro(Jogo jogo) {
+	public Tabuleiro(Jogo jogo) 
+	{
 		this.jogo = jogo;
 
-		try {
+		try 
+		{
 			imagemPeaoBranco = ImageIO.read(new File("Pecas/b_peao.png"));
 			imagemPeaoPreto = ImageIO.read(new File("Pecas/p_peao.png"));
 			imagemCavaloBranco = ImageIO.read(new File("Pecas/b_cavalo.png"));
@@ -54,26 +56,33 @@ public class Tabuleiro extends JPanel implements Observer {
 			imagemReiPreto = ImageIO.read(new File("Pecas/p_rei.png"));
 			imagemTorreBranca = ImageIO.read(new File("Pecas/b_torre.png"));
 			imagemTorrePreta = ImageIO.read(new File("Pecas/p_torre.png"));
-		} catch (IOException e) {
+		} 
+		catch (IOException e) 
+		{
 			System.out.println(e.getMessage());
 			System.exit(1);
 		}
 	}
-	public void paintComponent(Graphics g) {
+	public void paintComponent(Graphics g) 
+	{
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D) g;
 
 		int tamanho = 50;
 
 		// criando as casas do tabuleiro
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; j++) {
-				Rectangle2D casa = new Rectangle2D.Double(i * tamanho,
-						j * tamanho, tamanho, tamanho);
+		for (int i = 0; i < 8; i++) 
+		{
+			for (int j = 0; j < 8; j++) 
+			{
+				Rectangle2D casa = new Rectangle2D.Double (i * tamanho, j * tamanho, tamanho, tamanho);
 
-				if ((i + j) % 2 == 1) {
+				if ((i + j) % 2 == 1) 
+				{
 					g2d.setPaint(Color.BLACK);
-				} else {
+				} 
+				else 
+				{
 					g2d.setPaint(Color.WHITE);
 				}
 				g2d.fill(casa);
@@ -81,13 +90,15 @@ public class Tabuleiro extends JPanel implements Observer {
 		}
 
 		// iluminando as casas de movimentos possiveis
-		if (posicoesPossiveis != null) {
-			for (int i = 0; i < 8; i++) {
-				for (int j = 0; j < 8; j++) {
-					if (posicoesPossiveis[i][j] == true) {
-						Rectangle2D contorno = new Rectangle2D.Double(
-								i * tamanho, (7 - j) * tamanho, tamanho,
-								tamanho);
+		if (posicoesPossiveis != null) 
+		{
+			for (int i = 0; i < 8; i++) 
+			{
+				for (int j = 0; j < 8; j++) 
+				{
+					if (posicoesPossiveis[i][j] == true) 
+					{
+						Rectangle2D contorno = new Rectangle2D.Double( i * tamanho, (7 - j) * tamanho, tamanho, tamanho);
 
 						g2d.setStroke(new BasicStroke(3));
 						g2d.setPaint(Color.green);
@@ -98,74 +109,92 @@ public class Tabuleiro extends JPanel implements Observer {
 		}
 
 		// inserindo as imagens das peças
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; j++) {
+		for (int i = 0; i < 8; i++) 
+		{
+			for (int j = 0; j < 8; j++) 
+			{
 				Peca p = jogo.getPeca(i, j);
-				if (p instanceof Peao) {
-					if (p.getCor() == Cor.preto) {
-						g.drawImage(imagemPeaoPreto, i * tamanho,
-								(7 - j) * tamanho, tamanho, tamanho, null);
-					} else {
-						g.drawImage(imagemPeaoBranco, i * tamanho,
-								(7 - j) * tamanho, tamanho, tamanho, null);
+				
+				if (p instanceof Peao) 
+				{
+					if (p.getCor() == Cor.preto) 
+					{
+						g.drawImage(imagemPeaoPreto, i * tamanho, (7 - j) * tamanho, tamanho, tamanho, null);
+					} 
+					else 
+					{
+						g.drawImage(imagemPeaoBranco, i * tamanho, (7 - j) * tamanho, tamanho, tamanho, null);
 					}
 				}
-				if (p instanceof Torre) {
-					if (p.getCor() == Cor.preto) {
-						g.drawImage(imagemTorrePreta, i * tamanho,
-								(7 - j) * tamanho, tamanho, tamanho, null);
-					} else {
-						g.drawImage(imagemTorreBranca, i * tamanho,
-								(7 - j) * tamanho, tamanho, tamanho, null);
+				if (p instanceof Torre) 
+				{
+					if (p.getCor() == Cor.preto) 
+					{
+						g.drawImage(imagemTorrePreta, i * tamanho, (7 - j) * tamanho, tamanho, tamanho, null);
+					} 
+					else 
+					{
+						g.drawImage(imagemTorreBranca, i * tamanho, (7 - j) * tamanho, tamanho, tamanho, null);
 					}
 				}
-				if (p instanceof Bispo) {
-					if (p.getCor() == Cor.preto) {
-						g.drawImage(imagemBispoPreto, i * tamanho,
-								(7 - j) * tamanho, tamanho, tamanho, null);
-					} else {
-						g.drawImage(imagemBispoBranco, i * tamanho,
-								(7 - j) * tamanho, tamanho, tamanho, null);
+				if (p instanceof Bispo) 
+				{
+					if (p.getCor() == Cor.preto)
+					{
+						g.drawImage(imagemBispoPreto, i * tamanho, (7 - j) * tamanho, tamanho, tamanho, null);
+					}
+					else 
+					{
+						g.drawImage(imagemBispoBranco, i * tamanho, (7 - j) * tamanho, tamanho, tamanho, null);
 					}
 				}
-				if (p instanceof Rei) {
-					if (p.getCor() == Cor.preto) {
-						g.drawImage(imagemReiPreto, i * tamanho,
-								(7 - j) * tamanho, tamanho, tamanho, null);
-					} else {
-						g.drawImage(imagemReiBranco, i * tamanho,
-								(7 - j) * tamanho, tamanho, tamanho, null);
+				if (p instanceof Rei) 
+				{
+					if (p.getCor() == Cor.preto)
+					{
+						g.drawImage(imagemReiPreto, i * tamanho, (7 - j) * tamanho, tamanho, tamanho, null);
+					} 
+					else 
+					{
+						g.drawImage(imagemReiBranco, i * tamanho, (7 - j) * tamanho, tamanho, tamanho, null);
 					}
 				}
-				if (p instanceof Rainha) {
-					if (p.getCor() == Cor.preto) {
-						g.drawImage(imagemRainhaPreta, i * tamanho,
-								(7 - j) * tamanho, tamanho, tamanho, null);
-					} else {
-						g.drawImage(imagemRainhaBranca, i * tamanho,
-								(7 - j) * tamanho, tamanho, tamanho, null);
+				if (p instanceof Rainha) 
+				{
+					if (p.getCor() == Cor.preto) 
+					{
+						g.drawImage(imagemRainhaPreta, i * tamanho, (7 - j) * tamanho, tamanho, tamanho, null);
+					} 
+					else
+					{
+						g.drawImage(imagemRainhaBranca, i * tamanho, (7 - j) * tamanho, tamanho, tamanho, null);
 					}
 				}
-				if (p instanceof Cavalo) {
-					if (p.getCor() == Cor.preto) {
-						g.drawImage(imagemCavaloPreto, i * tamanho,
-								(7 - j) * tamanho, tamanho, tamanho, null);
-					} else {
-						g.drawImage(imagemCavaloBranco, i * tamanho,
-								(7 - j) * tamanho, tamanho, tamanho, null);
+				if (p instanceof Cavalo) 
+				{
+					if (p.getCor() == Cor.preto)
+					{
+						g.drawImage(imagemCavaloPreto, i * tamanho, (7 - j) * tamanho, tamanho, tamanho, null);
+					} 
+					else 
+					{
+						g.drawImage(imagemCavaloBranco, i * tamanho, (7 - j) * tamanho, tamanho, tamanho, null);
 					}
 				}
 			}
 		}
 	}
 
-	public void setPosicoesPossiveis(Boolean[][] posicoesPossiveis) {
+	public void setPosicoesPossiveis(Boolean[][] posicoesPossiveis)
+	{
 		this.posicoesPossiveis = posicoesPossiveis;
 		repaint();
 	}
 
-	public void update(Observable o, Object arg) {
-		if (((ArrayList<Object>) arg).get(0).equals("repaint")) {
+	public void update(Observable o, Object arg) 
+	{
+		if (((ArrayList<Object>) arg).get(0).equals("repaint")) 
+		{
 			repaint();
 		}
 	}
